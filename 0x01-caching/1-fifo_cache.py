e741da997d4b46269dad0366fea955033753473a
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-19 12:08:54 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,47 @@
+#!/usr/bin/python3
+""" FIFO Caching """
 
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.last_list(key)
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
+
+    def get(self, key):
+        """ Gets item from cache """
+        return self.cache_data.get(key, None)
+
+    def last_list(self, item):
+        """ Moves element to last """
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
+
+    def first_list(array):
+        """ Get first element """
+        return array[0] if array else None
