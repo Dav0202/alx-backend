80212f72be8f5101b1274461ea14488998fc93fe
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-25 13:32:58 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
 
+""" Module for Babel i18n """
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
