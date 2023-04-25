cf86d0dc55aded0cc45b18771e63c3dbe98e9674
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-25 21:53:12 +0100

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -14,21 +14,17 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
 app.config.from_object(Config)
-
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world() -> str:
     """ Renders a Basic Template """
     return render_template("3-index.html")
 
-
 @babel.localeselector
 def get_locale() -> str:
     """ Select a language translation """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 if __name__ == "__main__":
     app.run()
