fa2a10fc465e465fe8bb11da37e4205a56233494
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-25 21:58:41 +0100

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """ Module for Babel i18n
 """
-from flask_babel import Babel, _
-from flask import Flask, render_template, request, flash
+from flask_babel import Babel
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
 class Config(object):
-    """ Configuration Class for Babel """
-
+    """ Configuration Class for Babel
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -18,18 +18,18 @@ class Config(object):
 app.config.from_object(Config)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """ Renders a Basic Template
-    """
-    return render_template("3-index.html")
-
-
 @babel.localeselector
 def get_locale() -> str:
     """ Select a language translation
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """ Renders a Basic Template
+    """
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
