6e7b990ad1080e96eedf3da3d5aad64afcac80fb
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-23 09:51:02 +0100

    Update 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,21 @@
+import kue from 'kue';
 
+const queue = kue.createQueue();
+const jobFormat = {
+  phoneNumber: '95867448384',
+  message: 'This is to verify your phonr number is correct',
+};
+
+const queueName = 'push_notification_code';
+
+const job = queue.create(queueName, jobFormat).save((err) => {
+  if (!err) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
